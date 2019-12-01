5c815a5e4d700385c53f52b334b3ec2a59e72e5d
one-cent <55006589+one-cent@users.noreply.github.com>
2019-12-01 14:36:05 -0600

    meh;

--- app/src/main/java/com/example/illinoistrivia/MainActivity.java
+++ app/src/main/java/com/example/illinoistrivia/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -61,6 +62,17 @@ public class MainActivity extends AppCompatActivity {
                         }
                     });
                     builder.show();
+
+                } else if ((!q10.isChecked() || !q20.isChecked() || !q50.isChecked() || !all.isChecked()) && all.isChecked()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Please make sure all categories are selected.");
+                    builder.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            return;
+                        }
+                    });
+                    builder.show();
                 } else {
                     int questionPool = 0;
                     if (general.isChecked()) {
@@ -106,10 +118,11 @@ public class MainActivity extends AppCompatActivity {
                         builder.show();
                     } else {
                         QuestionsList.buildGameQuestions();
+                        Intent intent = new Intent(MainActivity.this, Game.class);
+                        startActivity(intent);
                     }
                 }
             }
         });
-
     }
 }
